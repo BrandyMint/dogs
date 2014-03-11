ddab416eeda406ba0c56efa520271f0f1792e97d
Alexander Mescheryakov <sibsfinx@ya.ru>
2014-03-11 16:29:02 +0400

    accelerate on mobile

--- config.rb
+++ config.rb
@@ -7,7 +7,7 @@ sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
-add_import_path "C:/project/dog/components"
+add_import_path "components"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
